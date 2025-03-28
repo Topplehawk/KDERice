27ccf986d309708d3b36843d8a9cf256187f8b3d
Topplehawk <104155019+Topplehawk@users.noreply.github.com>
2025-03-28 15:15:28 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt update
+sudo apt install qt6-style-kvantum qt6-style-kvantum-themessudo apt install kvantum -y
+sudo apt install latte-dock -y
+sudo apt update -y
